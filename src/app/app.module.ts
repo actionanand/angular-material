66686f1978fc8f5ca25a4d8f796416f7cf0e0fe1
Anand raja <46064269+actionanand@users.noreply.github.com>
2021-09-20 23:45:54 +0530

    mat table - initial set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,36 +11,52 @@ import {MatCardModule} from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import {MatTabsModule} from '@angular/material/tabs';
-import { TopMenuComponent } from './top-menu/top-menu.component';
 import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-
-
+import {CourseComponent} from "./course/course.component";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {CoursesService} from "./services/courses.service";
+import {HttpClientModule} from "@angular/common/http";
+import {CourseResolver} from "./services/course.resolver";
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
         AboutComponent,
-        TopMenuComponent,
+        CourseComponent,
         CoursesCardListComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        HttpClientModule,
         MatMenuModule,
         MatButtonModule,
         MatIconModule,
         MatCardModule,
         MatTabsModule,
-        AppRoutingModule,
         MatSidenavModule,
         MatListModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatInputModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatProgressSpinnerModule,
+        AppRoutingModule,
+
     ],
-    providers: [],
+    providers: [
+        CoursesService,
+        CourseResolver
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
